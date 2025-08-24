f125e46909c015cf7e57bf70e66e145117d4fb61
djefrey <djefrey@protonmail.com>
2025-08-24 12:08:18 +0200

    provide clrwl_setVertexOut in geometry stage
    I got some very bad crashes (segfaults) when using clrwl_out = clrwl_in[i]
    This may also serves as an hook in the future

--- common/src/main/resources/assets/colorwheel/flywheel/internal/instancing/iris_compat_geom.glsl
+++ common/src/main/resources/assets/colorwheel/flywheel/internal/instancing/iris_compat_geom.glsl
@@ -1,1 +1,14 @@
 vec4 clrwl_overlayColor = vec4(0.0);
+
+void clrwl_setVertexOut(int i)
+{
+    clrwl_out.flw_vertexPos = clrwl_in[i].flw_vertexPos;
+    clrwl_out.flw_vertexColor = clrwl_in[i].flw_vertexColor;
+    clrwl_out.flw_vertexTexCoord = clrwl_in[i].flw_vertexTexCoord;
+    clrwl_out.flw_vertexOverlay = clrwl_in[i].flw_vertexOverlay;
+    clrwl_out.flw_vertexLight = clrwl_in[i].flw_vertexLight;
+    clrwl_out.flw_vertexNormal = clrwl_in[i].flw_vertexNormal;
+    clrwl_out.clrwl_vertexTangent = clrwl_in[i].clrwl_vertexTangent;
+
+    clrwl_out.clrwl_debugIds = clrwl_in[i].clrwl_debugIds;
+}
