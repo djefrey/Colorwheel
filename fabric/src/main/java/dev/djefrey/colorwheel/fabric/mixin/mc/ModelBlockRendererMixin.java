c1f5e5ece7ebf75faded3f140a890945e7e20edc
djefrey <djefrey@protonmail.com>
2025-08-30 09:31:38 +0200

    fix fabric compile issue

--- fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/mc/ModelBlockRendererMixin.java
+++ fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/mc/ModelBlockRendererMixin.java
@@ -22,7 +22,7 @@ public class ModelBlockRendererMixin
             at = @At("HEAD"))
     private void injectBeingBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, RandomSource random, long seed, int packedOverlay, CallbackInfo ci)
     {
-        if (consumer instanceof ColorwheelBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
+        if (consumer instanceof BlockSensitiveBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
         {
             blockBuilder.beginBlock((short) WorldRenderingSettings.INSTANCE.getBlockStateIds().getInt(state),
                                     (byte) 0,
