e69af01f636e49bdb15466e5e5f146863e234bf6
djefrey <djefrey@protonmail.com>
2025-10-25 10:32:50 +0200

    fixes colored lights with forgified fabric

--- forge/src/main/java/dev/djefrey/colorwheel/forge/mixin/mc/ModelBlockRendererMixin.java
+++ forge/src/main/java/dev/djefrey/colorwheel/forge/mixin/mc/ModelBlockRendererMixin.java
@@ -13,34 +13,48 @@ import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ModelBlockRenderer.class)
+@Mixin(value = ModelBlockRenderer.class, priority = 300)
 public class ModelBlockRendererMixin
 {
-    @Inject(method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLnet/minecraft/util/RandomSource;JILnet/minecraftforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
-            at = @At("HEAD"),
-            remap = false)
-    private void injectBeingBlock(BlockAndTintGetter arg, BakedModel arg2, BlockState state, BlockPos pos, PoseStack arg5, VertexConsumer consumer, boolean bl, RandomSource arg7, long l, int i, ModelData modelData, RenderType renderType, CallbackInfo ci)
-    {
-        if (consumer instanceof BlockSensitiveBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
-        {
-            blockBuilder.beginBlock((short) WorldRenderingSettings.INSTANCE.getBlockStateIds().getInt(state),
-                                    (byte) 0,
-                                    pos.getX(), pos.getY(), pos.getZ());
-        }
-    }
+    @Unique
+    private boolean colorwheel$isFirstCall = true;
+
+    // This is required as Sodium (Fabric) and Indigo (Forge) injects a Mixin that cancels the call,
+    // which prevent cleanups done with @At("RETURN")
+    //
+    // The mixin priority is aggressive to ensure that this is called first,
+    // the method is never truly cancelled so that's okay
 
     @Inject(method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLnet/minecraft/util/RandomSource;JILnet/minecraftforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
-            at = @At("RETURN"),
+            at = @At("HEAD"),
+            cancellable = true,
             remap = false)
-    private void injectEndBlock(BlockAndTintGetter arg, BakedModel arg2, BlockState arg3, BlockPos arg4, PoseStack arg5, VertexConsumer consumer, boolean bl, RandomSource arg7, long l, int i, ModelData modelData, RenderType renderType, CallbackInfo ci)
+    private void injectBeginEndBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, RandomSource random, long seed, int packedOverlay, ModelData modelData, RenderType renderType, CallbackInfo ci)
     {
-        if (consumer instanceof BlockSensitiveBufferBuilder blockBuilder)
+        if (colorwheel$isFirstCall && consumer instanceof BlockSensitiveBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
         {
-            blockBuilder.endBlock();
+            blockBuilder.beginBlock((short) WorldRenderingSettings.INSTANCE.getBlockStateIds().getInt(state),
+                    (byte) 0,
+                    pos.getX(), pos.getY(), pos.getZ());
+
+            colorwheel$isFirstCall = false;
+
+            try
+            {
+                ((ModelBlockRenderer) (Object) this).tesselateBlock(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay, modelData, renderType);
+            }
+            finally
+            {
+                this.colorwheel$isFirstCall = true;
+
+                blockBuilder.endBlock();
+                ci.cancel();
+            }
         }
     }
 }
