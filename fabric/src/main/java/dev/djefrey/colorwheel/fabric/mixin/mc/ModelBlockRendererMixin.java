94539b984728df19d9ff82f21d5465cd343889d1
djefrey <djefrey@protonmail.com>
2026-05-01 10:51:37 +0200

    cleaner ModelBlockRenderer/BlockRenderDispatcher mixins

--- fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/mc/ModelBlockRendererMixin.java
+++ fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/mc/ModelBlockRendererMixin.java
@@ -1,9 +1,12 @@
 package dev.djefrey.colorwheel.fabric.mixin.mc;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.irisshaders.iris.shaderpack.materialmap.WorldRenderingSettings;
 import net.irisshaders.iris.vertices.BlockSensitiveBufferBuilder;
+import net.irisshaders.iris.vertices.ExtendedDataHelper;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
@@ -11,47 +14,28 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ModelBlockRenderer.class)
+@Mixin(value = ModelBlockRenderer.class, priority = 300)
 public class ModelBlockRendererMixin
 {
-    @Unique
-    private ThreadLocal<Boolean> colorwheel$isFirstCall = ThreadLocal.withInitial(() -> true);
-
-    // This is required as Sodium (Fabric) and Indigo (Forge) injects a Mixin that cancels the call,
-    // which prevent cleanups done with @At("RETURN")
-    //
-    // The mixin priority is aggressive to ensure that this is called first,
-    // the method is never truly cancelled so that's okay
-
-    @Inject(method = "tesselateBlock",
-            at = @At("HEAD"),
-            cancellable = true,
-            order = 300)
-    private void injectBeginEndBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, RandomSource random, long seed, int packedOverlay, CallbackInfo ci)
+    @WrapMethod(method = "tesselateBlock")
+    private void injectBeginEndBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer consumer, boolean checkSides, RandomSource random, long seed, int packedOverlay, Operation<Void> original)
     {
-        if (colorwheel$isFirstCall.get() && consumer instanceof BlockSensitiveBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
+        if (consumer instanceof BlockSensitiveBufferBuilder blockBuilder && WorldRenderingSettings.INSTANCE.getBlockStateIds() != null)
         {
-            blockBuilder.beginBlock((short) WorldRenderingSettings.INSTANCE.getBlockStateIds().getInt(state),
-                                    (byte) 0,
-                                    pos.getX(), pos.getY(), pos.getZ());
+            short id = (short) WorldRenderingSettings.INSTANCE.getBlockStateIds().getOrDefault(state, -1);
+            blockBuilder.beginBlock(id, ExtendedDataHelper.BLOCK_RENDER_TYPE,  pos.getX(), pos.getY(), pos.getZ());
+        }
 
-            colorwheel$isFirstCall.set(false);
-
-            try
+        try
+        {
+            original.call(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay);
+        }
+        finally
+        {
+            if (consumer instanceof BlockSensitiveBufferBuilder blockBuilder)
             {
-                ((ModelBlockRenderer) (Object) this).tesselateBlock(level, model, state, pos, poseStack, consumer, checkSides, random, seed, packedOverlay);
-            }
-            finally
-            {
-                this.colorwheel$isFirstCall.set(true);
-
                 blockBuilder.endBlock();
-                ci.cancel();
             }
         }
     }
