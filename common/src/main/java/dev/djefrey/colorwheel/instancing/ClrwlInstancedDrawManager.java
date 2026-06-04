ee2d53f848423ebd0466fbf39b48bb424fb327a5
djefrey <djefrey@protonmail.com>
2026-06-04 21:01:50 +0200

    stop reloading world on backend change

--- common/src/main/java/dev/djefrey/colorwheel/instancing/ClrwlInstancedDrawManager.java
+++ common/src/main/java/dev/djefrey/colorwheel/instancing/ClrwlInstancedDrawManager.java
@@ -726,7 +726,6 @@ public class ClrwlInstancedDrawManager extends ClrwlDrawManager<ClrwlInstancedIn
 	@Override
 	public void triggerFallback()
 	{
-		Minecraft.getInstance().levelRenderer.allChanged();
 	}
 
 	private String getShaderPackName()
