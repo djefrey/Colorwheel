496b7d1c25878990179c14c3f199739e504baa83
djefrey <djefrey@protonmail.com>
2026-06-06 17:13:57 +0200

    fix missed IncompatibleClassChangeError crash

--- fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/flw/v10000/UniversalMeshEmitterMixin.java
+++ fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/flw/v10000/UniversalMeshEmitterMixin.java
@@ -60,7 +60,7 @@ public abstract class UniversalMeshEmitterMixin implements ColorwheelBufferBuild
     @Override
     public void clrwlBeginBlock(short block, short renderType, byte lightEmission, boolean isTerrain, int posX, int posY, int posZ)
     {
-        beginBlock(block, renderType, posX, posY, posZ);
+        this.beginBlock(block, renderType, posX, posY, posZ);
         this.colorwheel$lightEmission = lightEmission;
     }
 
--- fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/flw/v10006/FabricMeshEmitterManagerMixin.java
+++ fabric/src/main/java/dev/djefrey/colorwheel/fabric/mixin/flw/v10006/FabricMeshEmitterManagerMixin.java
@@ -57,7 +57,7 @@ public abstract class FabricMeshEmitterManagerMixin implements VertexConsumer, C
     @Override
     public void clrwlBeginBlock(short block, short renderType, byte lightEmission, boolean isTerrain, int posX, int posY, int posZ)
     {
-        ColorwheelBufferBuilder.super.clrwlBeginBlock(block, renderType, lightEmission, isTerrain, posX, posY, posZ);
+        this.beginBlock(block, renderType, posX, posY, posZ);
         this.colorwheel$lightEmission = lightEmission;
     }
 
