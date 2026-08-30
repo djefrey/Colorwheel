701d9c42ceff2f746d24fb577c3d7df72b65658b
djefrey <djefrey@protonmail.com>
2026-08-30 11:13:38 +0200

    render translucent instances after translucent terrain

--- common/src/main/java/dev/djefrey/colorwheel/mixin/mc/LevelRendererMixin.java
+++ common/src/main/java/dev/djefrey/colorwheel/mixin/mc/LevelRendererMixin.java
@@ -23,7 +23,7 @@ public class LevelRendererMixin
     private ClientLevel level;
 
     @Inject(method = "renderLevel",
-            at = @At(value = "CONSTANT", args = "stringValue=translucent"),
+            at = @At(value = "CONSTANT", args = "stringValue=string"),
             order = 2000) // After Iris
     public void colorwheel$injectRenderTranslucents(PoseStack poseStack, float tickDelta, long finishNanoTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionMatrix, CallbackInfo ci)
     {
